42ceb0d347f8ca72e0e351e484927d7244b94ab3
Ilia Kupitman <KupitmanIL@vk.com>
2026-07-03 18:27:50 +0300

    First commit

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+import express from 'express';
 import forumRoutes from './routes/forumRoutes.js';
 
+dotenv.config();
+
+const port = process.env.PORT || 9000;
+
+const app = express();
+
 app.use(express.json());
+
 app.use(forumRoutes);
+
+app.use(req ,res) => res.status(404).type('text/plain; charset=utf-8').send('Not found')
+
+app.listen(port, () => console.log(`Server running on port ${port}. Press Ctrl+C to stop.`));
